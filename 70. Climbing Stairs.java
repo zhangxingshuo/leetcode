2b0c0e2f5507cdbcd0bb3014f1853e7b00ea9be9
Andy Zhang <axzhang@hmc.edu>
2017-07-29 23:14:17 -0400

    Update 70. Climbing Stairs.java

--- 70. Climbing Stairs.java
+++ 70. Climbing Stairs.java
@@ -1,5 +1,8 @@
 public class Solution {
     public int climbStairs(int n) {
+        // analogous to fibonacci, how many ways to tile a 1xn board with
+        // either 1 or 2 length domino pieces
+        
         // dynamic programming
         // int[] fib = new int[n+1];
         // fib[0] = 1;
@@ -12,6 +15,7 @@ public class Solution {
     }
     
     public int climbStairsBinet(int n) {
+        // Binet's formula
         double phi = (1 + Math.sqrt(5)) / 2;
         double phi_bar = (1 - Math.sqrt(5)) / 2;
         return (int) (1 / Math.sqrt(5) * (Math.pow(phi, n) - Math.pow(phi_bar, n)));
