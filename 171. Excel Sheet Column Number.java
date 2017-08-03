2d46b88200bf4b4475f97bf0ff9b12bdac20160b
Andy Zhang <axzhang@hmc.edu>
2017-08-03 12:39:07 -0400

    Update 171. Excel Sheet Column Number.java

--- 171. Excel Sheet Column Number.java
+++ 171. Excel Sheet Column Number.java
@@ -2,6 +2,7 @@ public class Solution {
     public int titleToNumber(String s) {
         int num = 0;
         for (int i = 0; i < s.length(); i++) {
+            // convert base 26 to decimal
             int val = s.charAt(i) - 'A' + 1;
             num = num * 26 + val;
         }
