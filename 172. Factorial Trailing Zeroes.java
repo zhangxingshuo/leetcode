563bd1a810e5537f3493c801d75b8554983cda9f
Andy Zhang <axzhang@hmc.edu>
2017-08-03 12:39:47 -0400

    Update 172. Factorial Trailing Zeroes.java

--- 172. Factorial Trailing Zeroes.java
+++ 172. Factorial Trailing Zeroes.java
@@ -3,6 +3,8 @@ public class Solution {
         // recursive
         // if (n == 0) return 0;
         // else return n / 5 + trailingZeroes(n / 5);
+        
+        // count multiples of powers of 5
         int count = 0;
         while (n > 0) {
             count += n / 5;
