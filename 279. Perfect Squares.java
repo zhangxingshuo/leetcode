5527340c28e58edb3845f16fbce5c9f6b1790441
Andy Zhang <axzhang@hmc.edu>
2017-08-03 11:36:22 -0400

    Add files via upload

---
+++ 279. Perfect Squares.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int numSquares(int n) {
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, n+1);
+        arr[0] = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int s = 1;
+            while (s*s <= i) {
+                arr[i] = Math.min(arr[i], 1 + arr[i - s*s]);
+                s++;
+            }
+        }
+        return arr[n];
+    }
+}
