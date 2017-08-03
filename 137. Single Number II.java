67d990281bdcce3a8b374ae5805ff9665bc7ab4a
Andy Zhang <axzhang@hmc.edu>
2017-08-03 12:36:59 -0400

    Update 137. Single Number II.java

--- 137. Single Number II.java
+++ 137. Single Number II.java
@@ -1,11 +1,13 @@
 public class Solution {
     public int singleNumber(int[] nums) {
+        // make a three-counter
         int a = 0, b = 0;
         for (int num : nums) {
             int temp_a = (~a&b&num) | (a&~b&~num);
             b = (~a&~b&num) | (~a&b&~num);
             a = temp_a;
         }
+        // return number whose count is 1
         return a|b;
     }
 }
