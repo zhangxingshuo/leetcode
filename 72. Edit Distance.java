7fe4e2f0e3e3d75e7eeb25606d0283fbb0129aa3
Andy Zhang <axzhang@hmc.edu>
2017-08-03 13:00:33 -0400

    Update 72. Edit Distance.java

--- 72. Edit Distance.java
+++ 72. Edit Distance.java
@@ -1,13 +1,19 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
+        // dynamic programming
         int n = word1.length();
         int m = word2.length();
         int[][] dist = new int[n+1][m+1];
         for (int i = 0; i <= n; ++i) {
             for (int j = 0; j <= m; ++j) {
+                // if either string empty, need to insert every char of other string
                 if (i == 0) dist[i][j] = j;
                 else if (j == 0) dist[i][j] = i;
+                
+                // if chars are equal, no edit necessary
                 else if (word1.charAt(i-1) == word2.charAt(j-1)) dist[i][j] = dist[i-1][j-1];
+                
+                // take the min of replacement, insertion, and deletion
                 else {
                     int rep = dist[i-1][j-1];
                     int ins = dist[i][j-1];
